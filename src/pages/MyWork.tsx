5fd6cbf8f12da5c38b8265fd8ce5ae0905e67a31
fareskebeh <fares.kebbeh@gmail.com>
2025-07-21 07:50:24 -0700

    feat: my work structure

--- src/pages/MyWork.tsx
+++ src/pages/MyWork.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import GitHubCalendar from "react-github-calendar"
+import Timeline from "./Timeline"
 
 type Props = {}
 
 const MyWork = (props: Props) => {
   return (
-    <div>MyWork</div>
+    <div className="space-y-4">
+      <p className="text-3xl font-bold sticky top-10 bg-white left-0 right-0 ">My Work</p>
+      <p className="text-xl">Here's a peek at my GitHub contribution history, and below is a timeline of all my projects</p>
+      <div className="">
+        <GitHubCalendar colorScheme="dark" username="fareskebeh"/>
+      </div>
+      <Timeline/>
+    </div>
   )
 }
 
