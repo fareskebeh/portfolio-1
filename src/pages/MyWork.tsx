17696c240689a6792c20557037112ea1ee2f86c8
fareskebeh <fares.kebbeh@gmail.com>
2025-07-21 07:51:01 -0700

    fix: no need for props

--- src/pages/MyWork.tsx
+++ src/pages/MyWork.tsx
@@ -1,9 +1,8 @@
 import GitHubCalendar from "react-github-calendar"
 import Timeline from "./Timeline"
 
-type Props = {}
 
-const MyWork = (props: Props) => {
+const MyWork = () => {
   return (
     <div className="space-y-4">
       <p className="text-3xl font-bold sticky top-10 bg-white left-0 right-0 ">My Work</p>
