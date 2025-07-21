85507dcee8acdf1f3149a03f9780d02a473be604
fareskebeh <fares.kebbeh@gmail.com>
2025-07-21 04:05:59 -0700

    feat: defined routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom"
+import About from "./pages/About"
+import Card from "./pages/Card"
+import ContactMe from "./pages/ContactMe"
+import MyWork from "./pages/MyWork"
+import Nav from "./components/Nav"
 
 const App = () => {
   return (
     <div>
-      
+    <Router>
+      <Nav/>
+      <Card/>
+
+      <Routes>
+        
+        <Route path="/about" element={< About/>}/>
+        <Route path="/my-work" element={< MyWork/>}/>
+        <Route path="/contact-me" element={< ContactMe/>}/>
+        <Route path="*" element={<Navigate to="/about"/>}/>
+      </Routes>
+    </Router>
     </div>
   )
 }
---
+++ src/pages/MyWork.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const MyWork = (props: Props) => {
+  return (
+    <div>MyWork</div>
+  )
+}
+
+export default MyWork
