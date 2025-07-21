b237fe1ffa4ebddfed7931888ba2cd87f801f75d
fareskebeh <fares.kebbeh@gmail.com>
2025-07-21 07:51:18 -0700

    feat: timeline layout pre-styled

---
+++ src/pages/Timeline.tsx
@@ -0,0 +1,108 @@
+import sword from "../assets/icons/sword.png";
+import star from "../assets/icons/star.png";
+import reactIcon from "../assets/icons/react.svg";
+import pythonIcon from "../assets/icons/python.svg";
+import supabaseIcon from "../assets/icons/supabase.svg";
+import jsIcon from "../assets/icons/js.svg";
+import tsIcon from "../assets/icons/ts.svg";
+import djangoIcon from "../assets/icons/django.svg";
+import tailwindIcon from "../assets/icons/tailwind.svg";
+
+type Project = {
+  name: string;
+  description: string;
+  technologies: string[];
+  startDate: string;
+  image: string;
+  state: string;
+  link: string;
+  purpose: string;
+};
+
+const Timeline = () => {
+  const projects: Project[] = [
+    {
+      name: "Gold Street",
+      description:
+        "I built an SaaS for a trading agency, with private channel handling using TelegramAPI",
+      technologies: [reactIcon, tailwindIcon, supabaseIcon, pythonIcon],
+      startDate: "27-June-2025",
+      image: "goldstreet",
+      state: "Completed",
+      link: "https://github.com/fareskebeh/goldstreet",
+      purpose: "prod",
+    },
+    {
+      name: "Tutorme",
+      description:
+        "I'm currently building the frontend for a tutoring web app, the app contains a chat feature, so far my biggest project",
+      technologies: [reactIcon, tailwindIcon, tsIcon],
+      startDate: "18-June-2025",
+      image: "tutorme",
+      state: "In Progress..",
+      link: "https://github.com/fareskebeh/tutorme",
+      purpose: "prod",
+    },
+    {
+      name: "Rigsy",
+      description:
+        "My only scalable full-stack app, currently abandoned due to other current overriding projects",
+      technologies: [reactIcon, tailwindIcon, djangoIcon],
+      startDate: "17-May-2025",
+      image: "rigsy",
+      state: "Stopped",
+      link: "https://github.com/fareskebeh/ecom",
+      purpose: "prod",
+    },
+    {
+      name: "Blog app",
+      description:
+        "My first full-stack app :), a blog app where you can read, comment and share posts, currently missing authentication and proper formatting",
+      technologies: [reactIcon, tailwindIcon, djangoIcon],
+      startDate: "08-April-2025",
+      image: "blog",
+      state: "In Progress..",
+      link: "https://github.com/fareskebeh/blog-app",
+      purpose: "prac",
+    },
+    {
+      name: "Matrix Component",
+      description:
+        "a lightweight component that displays flickering 0s and 1s, made this project while meddling with JavaScript, you can install it from npm :)",
+      technologies: [jsIcon, tailwindIcon],
+      startDate: "12-May-2025",
+      image: "matrix",
+      state: "Completed",
+      link: "https://github.com/fareskebeh/matrix-component",
+      purpose: "prac",
+    },
+  ];
+
+  return (
+    <div className="text-xl space-y-8">
+      <div>
+        NOTE: The projects denoted by a{" "}
+        <img className="inline-block w-10" src={sword} /> icon are practice
+        projects, while projects denoted by a{" "}
+        <img className="w-8 inline-block" src={star} /> icon are projects for
+        external clients
+      </div>
+
+      <div className="border-l-4 pl-4">
+        {projects.map((p, index) => (
+          <div className="p-4 space-y-4" key={index}>
+            <p className="text-2xl font-bold">{p.name}</p>
+            <p>{p.description}</p>
+            <div className="*:w-15 flex gap-4">
+            {p.technologies.map((t, index) => (
+              <img key={index} src={t} />
+            ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Timeline;
