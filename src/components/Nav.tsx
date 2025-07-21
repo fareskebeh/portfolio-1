9b00647bda81908dee750f33f639db5b803328a7
fareskebeh <fares.kebbeh@gmail.com>
2025-07-21 06:53:32 -0700

    fix: fixed nav sliding below content

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const Nav = () => {
   return (
-    <div className=" sm:text-xl fixed top-0 left-0 right-0 flex *:tracking-wider justify-around sm:justify-center sm:gap-40 p-4">
+    <div className=" sm:text-xl fixed top-0 z-999 left-0 right-0 flex *:tracking-wider justify-around sm:justify-center sm:gap-40 p-4">
       <Link to="/about">ABOUT ME</Link>
       <Link to="/my-work">MY WORK</Link>
       <Link to="/contact-me">CONTACT ME</Link>
