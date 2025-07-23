93f0a4de602f08c93085756d8d16fc493ba4fa3f
fareskebeh <fares.kebbeh@gmail.com>
2025-07-23 06:17:36 -0700

    feat: navigation bar styling

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,12 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 const Nav = () => {
+  const location = useLocation()
   return (
-    <div className=" sm:text-xl fixed top-0 z-999 left-0 right-0 flex *:tracking-wider justify-around sm:justify-center sm:gap-40 p-4">
-      <Link to="/about">ABOUT ME</Link>
-      <Link to="/my-work">MY WORK</Link>
-      <Link to="/contact-me">CONTACT ME</Link>
+    <div className=" bg-black *:hover:opacity-80 *:transition duration-200 fixed top-0 z-999 left-0 right-0 flex *:tracking-wider justify-around sm:justify-center sm:gap-40 p-4">
+      <Link className={location.pathname==="/about" ?      "font-bold text-white scale-110" : "text-neutral-500"} to="/about">ABOUT ME</Link>
+      <Link className={location.pathname==="/my-work" ?    "font-bold text-white scale-110" : "text-neutral-500"} to="/my-work">MY WORK</Link>
+      <Link className={location.pathname==="/contact-me" ? "font-bold text-white scale-110" : "text-neutral-500"} to="/contact-me">CONTACT ME</Link>
     </div>
   )
 }
