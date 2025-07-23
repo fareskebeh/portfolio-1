b531f514249a5fa92688fd8b2c77f86aa0193021
fareskebeh <fares.kebbeh@gmail.com>
2025-07-23 06:53:05 -0700

    fix: fixed padding and bg color

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,9 @@ const App = () => {
       <Card/>
 
       <Routes>
-        <Route path="/about" element={<div      className="sm:pt-20 px-8 sm:absolute right-0 top-0 bottom-0 left-1/2"> <About/> </div> }/>
-        <Route path="/my-work" element=   {<div className="sm:pt-20 px-8 sm:absolute right-0 top-0 bottom-0 left-1/2">< MyWork/></div>}/>
-        <Route path="/contact-me" element={<div className="sm:pt-20 px-8 sm:absolute flex items-center right-0 top-0 bottom-0 left-1/2">< ContactMe/></div>}/>
+        <Route path="/about" element={<div      className="sm:py-20 px-8 sm:absolute bg-black right-0 top-0 bottom-0 left-1/2"> <About/> </div> }/>
+        <Route path="/my-work" element=   {<div className="sm:py-20 px-8 sm:absolute bg-black right-0 top-0 bottom-0 left-1/2">< MyWork/></div>}/>
+        <Route path="/contact-me" element={<div className="sm:py-20 px-8 sm:absolute bg-black flex items-center right-0 top-0 bottom-0 left-1/2">< ContactMe/></div>}/>
         <Route path="*" element={<Navigate to="/about"/>}/>
       </Routes>
     </Router>
