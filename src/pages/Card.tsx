c8458ffee4140eca413e4aefa83b375d11a5d4bf
fareskebeh <fares.kebbeh@gmail.com>
2025-07-23 06:20:37 -0700

    feat: card basic styling

--- src/pages/Card.tsx
+++ src/pages/Card.tsx
@@ -29,20 +29,20 @@ const Card = () => {
   ]
 
   return (
-    <div className="pt-14 px-8 sm:fixed top-0 h-100 sm:h-auto left-0 bottom-0 right-1/2 flex flex-col justify-center">
+    <div className="bg-black text-white pt-14 px-8 sm:fixed top-0 h-100 sm:h-auto left-0 bottom-0 right-1/2 flex flex-col justify-center">
       
       <div className="space-y-8 sm:space-y-12">
         <div className="space-y-2 sm:space-y-4">
           <p className="font-black text-5xl sm:text-6xl">Fares Kebeh</p>
           <p className="text-2xl font-bold">Full-Stack Engineer</p>
-          <p className="text-xl">I build responsive, performant web and mobile apps</p>
+          <p className="text-xl text-neutral-400">I build responsive, performant web and mobile apps</p>
         </div>
 
         <div className=" flex gap-8">
           {
             socials.map((social,index)=> (
                 <a key={index} target="_blank" href={social.link}>
-                  <img className="w-10" src={social.icon}/>
+                  <img className="w-10 hover:opacity-80 transition duration-200 invert-90" src={social.icon}/>
                 </a>
             ))
           }
