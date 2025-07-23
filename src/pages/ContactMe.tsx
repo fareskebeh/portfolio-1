9e05c20ad8cab72fefa7aef828262ce778f509ce
fareskebeh <fares.kebbeh@gmail.com>
2025-07-23 07:11:04 -0700

    feat: styled the timeline and work section

--- src/pages/ContactMe.tsx
+++ src/pages/ContactMe.tsx
@@ -3,10 +3,10 @@
 const ContactMe = () => {
   return (
     <div className='space-y-4'>
-      <p className="text-3xl font-bold sticky top-10 bg-white left-0 right-0 ">Contact Me</p>
-      <p className="text-xl">Do you like any of my projects, or want me to add value to your team? Contact me through the following methods:</p>
+      <p className="text-3xl font-bold sticky top-14 bg-black text-white left-0 right-0 ">Contact Me</p>
+      <p className="text-xl text-neutral-400">Do you like any of my projects, or want me to add value to your team? Contact me through the following methods:</p>
 
-      <ul className='text-xl list-disc pl-8 space-y-2'>
+      <ul className='text-xl text-neutral-400 list-disc pl-8 space-y-2'>
         <li>Mail me: <a>fares_kebbeh@outlook.com</a></li>
         <li>Reach out to my social media listed above</li>
       </ul>
