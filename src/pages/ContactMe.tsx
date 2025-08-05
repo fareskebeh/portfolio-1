828f872274e4fce65ceab4c7b90f413b3758ac3a
fareskebeh <fares.kebbeh@gmail.com>
2025-08-05 12:19:52 -0700

    fix: slight adjustments for validation

--- src/pages/ContactMe.tsx
+++ src/pages/ContactMe.tsx
@@ -28,17 +28,14 @@ const ContactMe = () => {
 
   useEffect(() => {
     const handler = (e: BeforeUnloadEvent) => {
-      // If submission was successful, allow unload without warning
       if (output.error === false) return;
 
-      // Otherwise, if form has unsaved input, block unload
       if (
         inquiry.name.trim() !== "" ||
         inquiry.email.trim() !== "" ||
         inquiry.message.trim() !== ""
       ) {
         e.preventDefault();
-        e.returnValue = ""; // Required for Chrome
       }
     };
 
@@ -53,6 +50,19 @@ const ContactMe = () => {
   const sendMessage = async (e: React.FormEvent) => {
     e.preventDefault();
     setOutput({ ...output, message: "", loading: true });
+
+    if(
+      inquiry.name.trim()==="" ||
+      inquiry.email.trim()==="" ||
+      inquiry.message.trim()==="" 
+    ) {
+      setOutput({message: "Missing required fields.", error:true, loading: false})
+      return;
+    }
+    if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(inquiry.email) === false) {
+      setOutput({message: "Invalid email format.", error:true, loading: false})
+      return;
+    }
 
     const formData = new FormData();
 
@@ -119,11 +129,10 @@ const ContactMe = () => {
         onSubmit={sendMessage}
         className=" flex flex-col **:caret-white gap-2 items-stretch"
       >
-        <div className="flex flex-col  sm:flex-row gap-2 *:border *:p-2 text-xl *:flex-1">
+        <div className="flex flex-col sm:flex-row gap-2 *:border *:p-2 text-xl *:flex-1">
           <input
             onChange={(e) => setInquiry({ ...inquiry, name: e.target.value })}
             value={inquiry.name}
-            required
             name="name"
             className="border-neutral-800 sm:border-neutral-700 sm:bg-neutral-800/40 bg-neutral-900/60 rounded-xl text-white outline-none backdrop-blur-2xl"
             placeholder="Your Name..."
@@ -132,7 +141,6 @@ const ContactMe = () => {
           <input
             onChange={(e) => setInquiry({ ...inquiry, email: e.target.value })}
             value={inquiry.email}
-            required
             name="email"
             className="border-neutral-800 sm:border-neutral-700 sm:bg-neutral-800/40 bg-neutral-900/60 rounded-xl text-white outline-none backdrop-blur-2xl invalid:border-red-700/50 invalid:bg-red-700/20 transition duration-300"
             placeholder="Your Email..."
@@ -142,10 +150,9 @@ const ContactMe = () => {
         <textarea
           onChange={(e) => setInquiry({ ...inquiry, message: e.target.value })}
           value={inquiry.message}
-          required
           name="message"
           placeholder="Your Message..."
-          className="w-full p-2 text-white text-xl h-40 resize-none outline-none border border-neutral-800 sm:border-neutral-700 bg-neutral-900/60 rounded-xl"
+          className="w-full p-2 backdrop-blur-xl text-white text-xl h-40 resize-none outline-none border border-neutral-800 sm:border-neutral-700 bg-neutral-900/60 rounded-xl"
         ></textarea>
         <div className="flex flex-col items-start gap-2 mb-12">
           <ReCAPTCHA
