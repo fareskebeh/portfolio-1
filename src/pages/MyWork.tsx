7a01789f0b9387540ba5baedafa5ff6920d612a8
fareskebeh <fares.kebbeh@gmail.com>
2025-08-16 12:25:39 +0300

    fix: mywork header appearing under elements

--- src/pages/MyWork.tsx
+++ src/pages/MyWork.tsx
@@ -21,7 +21,7 @@ const MyWork = () => {
 
   return (
     <div className="space-y-4">
-      <p className="text-3xl font-bold sticky top-14 z-20 bg-[#494949] text-white left-0 right-0 ">My Work</p>
+      <p className="text-3xl font-bold sticky top-14 z-999 bg-[#494949] text-white left-0 right-0 ">My Work</p>
       <p className="text-xl text-neutral-400">Here's a peek at my latest GitHub contribution history, and below is a timeline of all my projects</p>
       <div className="border border-neutral-600 bg-neutral-700 p-2 rounded-xl *:text-white flex">
         <GitHubCalendar colorScheme="dark" transformData={filterContributions as any} username="fareskebeh"/>
